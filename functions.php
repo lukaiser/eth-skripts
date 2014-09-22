d1ee017df64ec9553b21c942e254d6468a79bd0c
Lukas <Kaiser>
2014-09-22 17:07:20 +0200

    Add filter for numbers

--- functions.php
+++ functions.php
@@ -10,6 +10,8 @@ function fitzgerald_enqueue_styles() {
 
 add_action( 'wp_print_styles', 'fitzgerald_enqueue_styles' );
 
+add_filter("pb_lists_add_numbers_to_list_elements", function(){return true;});
+
 /**
  * Returns an html blog of meta elements 
  * 
